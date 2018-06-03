b320a5ad8ac8940cf8c07d52b48aab89db8db68f
Joaquín Jiménez García <joaquin@nostromo.home>
2018-06-03 19:02:26 +0200

    Completed Neruon and NeuralLayer

--- src/neuralnet/NeuralLayer.java
+++ src/neuralnet/NeuralLayer.java
@@ -13,4 +13,92 @@ public abstract class NeuralLayer {
     protected ArrayList<Double> input;
     protected ArrayList<Double> output;
     protected int numberOfInputs;
+    
+    public NeuralLayer(int numberofneurons){
+        this.numberOfNeuronsInLayer = numberofneurons;
+        neuron = new ArrayList<>(numberofneurons);
+        output = new ArrayList<>(numberofneurons);
+    }
+    
+    public NeuralLayer(int numberofneurons, IActivationFunction iaf){
+        this.numberOfNeuronsInLayer = numberofneurons;
+        this.activationFnc = iaf;
+        neuron = new ArrayList<>(numberofneurons);
+        output = new ArrayList<>(numberofneurons);
+    }
+    
+    protected void init(){
+        if(numberOfNeuronsInLayer >= 0){
+            for(int i = 0; i<numberOfNeuronsInLayer; i++){
+                try{
+                    neuron.get(i).setActivationFunction(activationFnc);
+                    neuron.get(i).init();
+                }
+                catch(IndexOutOfBoundsException iobe){
+                    neuron.add(new Neuron(numberOfInputs,activationFnc));
+                    neuron.get(i).init();
+                }
+            }
+        }
+    }
+    
+    protected void calc(){
+        if(input != null && neuron != null){
+            for(int i = 0; i<numberOfNeuronsInLayer; i++){
+                neuron.get(i).setInputs(this.input);
+                neuron.get(i).calc();
+                try{
+                    output.set(i,neuron.get(i).getOutput());
+                }
+                catch(IndexOutOfBoundsException iobe){
+                    output.add(neuron.get(i).getOutput());
+                }
+            }
+        }
+    }
+    
+    public int getNumberOfNeuronsInLayer(){
+        return numberOfNeuronsInLayer;
+    }
+    
+    public ArrayList<Neuron> getListOfNeurons(){
+        return neuron;
+    }
+    
+    protected NeuralLayer getPreviousLayer(){
+        return previousLayer;
+    }
+    
+    protected NeuralLayer getNextLayer(){
+        return nextLayer;
+    }
+    
+    protected void setPreviousLayer(NeuralLayer layer){
+        previousLayer = layer;
+    }
+    
+    protected void setNextLayer(NeuralLayer layer){
+        nextLayer = layer;
+    }
+    
+    protected void setInputs(ArrayList<Double> inputs){
+        this.numberOfInputs=inputs.size();
+        this.input=inputs;
+    }
+    
+    protected ArrayList<Double> getOutputs(){
+        return output;
+    }
+    
+    protected Neuron getNeuron(int i){
+        return neuron.get(i);
+    }
+    
+    protected void setNeuron(int i, Neuron _neuron){
+        try{
+            this.neuron.set(i, _neuron);
+        } catch(IndexOutOfBoundsException iobe){
+            this.neuron.add(_neuron);
+        }
+    }
 }
--- src/neuralnet/Neuron.java
+++ src/neuralnet/Neuron.java
@@ -14,15 +14,21 @@ public class Neuron {
     protected Double bias = 1.0;
     private IActivationFunction activationFunction;
     
+    public Neuron(int numberofinputs){
+        numberOfInputs = numberofinputs;
+        weight = new ArrayList<>(numberofinputs+1);
+        input = new ArrayList<>(numberofinputs);
+    }
+    
     public Neuron(int numberofinputs, IActivationFunction iaf){
-        numberOfInputs=numberofinputs;
-        weight=new ArrayList<>(numberofinputs+1);
-        input=new ArrayList<>(numberofinputs);
+        numberOfInputs = numberofinputs;
+        weight = new ArrayList<>(numberofinputs+1);
+        input = new ArrayList<>(numberofinputs);
         activationFunction=iaf;
     }
     
     public void init(){
-        for(int i=0;i<=numberOfInputs;i++){
+        for(int i = 0; i <= numberOfInputs; i++){
           double newWeight = RandomNumberGenerator.GenerateNext();
           try{
             this.weight.set(i, newWeight);
@@ -35,17 +41,103 @@ public class Neuron {
     
    
     public void calc(){
-        outputBeforeActivation=0.0;
-        if(numberOfInputs>0){
-          if(input!=null && weight!=null){
-            for(int i=0;i<=numberOfInputs;i++){
-              outputBeforeActivation+=(i==numberOfInputs?bias:input.get(i))*weight.get(i);
+        outputBeforeActivation = 0.0;
+        if(numberOfInputs > 0){
+          if(input != null && weight != null){
+            for(int i = 0; i <= numberOfInputs; i++){
+              outputBeforeActivation += (i ==numberOfInputs?bias:input.get(i))*weight.get(i);
             } 
           }
         }
     
-        output=activationFunction.calc(outputBeforeActivation);
+        output = activationFunction.calc(outputBeforeActivation);
     }
     
+    public void setInputs(double [] values){
+        if(values.length == numberOfInputs){
+            for(int i = 0; i < numberOfInputs; i++){
+                try{
+                    input.set(i, values[i]);
+                } catch(IndexOutOfBoundsException iobe){
+                    input.add(values[i]);
+                }
+            }
+        }
+    }
     
+    public void setInputs(ArrayList<Double> values){
+        if(values.size() == numberOfInputs){
+            input = values;
+        }
+    }
+
+    public ArrayList<Double> getArrayInputs(){
+        return input;
+    }
+    
+    public double[] getInputs(){
+        double[] inputs = new double[numberOfInputs];
+        for (int i = 0; i < numberOfInputs; i++){
+            inputs[i] = this.input.get(i);
+        }
+        return inputs;
+    }
+    
+    public void setInput(int i, double value){
+        if(i >= 0 && i < numberOfInputs){
+            try{
+                input.set(i, value);
+            }
+            catch(IndexOutOfBoundsException iobe){
+                input.add(value);
+            }
+        }
+    }
+    
+    public double getInput(int i){
+        return input.get(i);
+    }
+    
+    public double[] getWeights(){
+        double[] weights = new double[numberOfInputs +1];
+        for(int i = 0; i <= numberOfInputs; i++){
+            weights[i]=weight.get(i);
+        }
+        
+        return weights;
+    }
+    
+    public ArrayList<Double> getArrayWeights(){
+        return weight;
+    }
+    
+    public void updateWeight(int i, double value){
+        if(i >= 0 && i <= numberOfInputs){
+            weight.set(i, value);
+        }
+    }
+    
+    public int getNumberOfInputs(){
+        return this.numberOfInputs;
+    }
+    
+    public void setWeight(int i, double value) throws NeuralException{
+        if(i >= 0 && i < numberOfInputs){
+            this.weight.set(i, value);
+        } else {
+            throw new NeuralException("Invalid weight index");
+        }
+    }
+    
+    public double getOutput(){
+        return output;
+    }
+    
+    public void setActivationFunction(IActivationFunction iaf){
+        this.activationFunction = iaf;
+    }
+    
+    public double getOutputBeforeActivation(){
+        return outputBeforeActivation;
+    }
 }
